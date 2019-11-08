068059e2c13d307d4951901bb09e00e4709f9310
Marc Fernan H. Abadines <marc.abadines@opsolutions.biz>
2019-11-08 11:16:19 +0800

    Initial Commit

---
+++ APICallsTests/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  APICallsTests
+//
+//  Created by OPSolutions on 08/11/2019.
+//  Copyright © 2019 OPSolutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
